6fb78676b4bf5486e096367336e4c15a3da9c040
Nguyễn Duy Hưng <124774263+yudgunH@users.noreply.github.com>
2024-12-16 12:25:25 +0700

    Toast để thông báo lỗi cho login

---
+++ frontend/src/lib/errorMessages.js
@@ -0,0 +1,9 @@
+const errorMessages = {
+    "INVALID_EMAIL": "Email không hợp lệ.",
+    "INVALID_PASSWORD": "Mật khẩu sai. Vui lòng thử lại.",
+    "USER_NOT_FOUND": "Tài khoản không tồn tại.",
+    "SERVER_ERROR": "Đã xảy ra lỗi trên server. Vui lòng thử lại sau.",
+  };
+  
+  export default errorMessages;
+  
