11fcfb550c61ed4b142d6c9f7069fab2f6963161
oceantran27 <daiduong3a1@gmail.com>
2024-12-20 14:13:00 +0700

    update login api

--- backend/src/controllers/login/index.js
+++ backend/src/controllers/login/index.js
@@ -9,13 +9,13 @@ import {
   dbGetCustomerById,
 } from "../../services/users/customer.service";
 
+import { dbGetAdminById } from "../../services/users/admin.service";
+
 const firebaseAuth = getAuth(firebase);
 
 export const handleLogin = async (req, res) => {
   try {
     const { email, password, provider } = req.body;
-
-    console.log("ress URL:", req.originalUrl);
 
     let user;
     if (provider === "google") {
@@ -51,6 +51,71 @@ export const handleLogin = async (req, res) => {
   } catch (error) {
     res.status(400).send({
       message: error.message,
+    });
+  }
+};
+
+export const handleCustomerLogin = async (req, res) => {
+  try {
+    const { email, password, provider } = req.body;
+    if (!email || !password) {
+      return res.status(400).send("Email and password are required");
+    }
+    const userCredential = await signInWithEmailAndPassword(
+      firebaseAuth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    const userId = user.uid;
+
+    const promiseGetCustomer = await dbGetCustomerById(userId);
+    const promiseGetIdToken = await user.getIdToken();
+    const [customer, idToken] = await Promise.all([
+      promiseGetCustomer,
+      promiseGetIdToken,
+    ]);
+    res.status(200).send({
+      message: "Login successful",
+      token: idToken,
+      provider: provider || "email",
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Email or password is incorrect",
+    });
+  }
+};
+
+export const handleAdminLogin = async (req, res) => {
+  try {
+    const { email, password, provider } = req.body;
+    if (!email || !password) {
+      return res.status(400).send("Email and password are required");
+    }
+    const userCredential = await signInWithEmailAndPassword(
+      firebaseAuth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    const userId = user.uid;
+    const promiseGetAdmin = await dbGetAdminById(userId);
+    const promiseGetIdToken = await user.getIdToken();
+
+    const [admin, idToken] = await Promise.all([
+      promiseGetAdmin,
+      promiseGetIdToken,
+    ]);
+
+    res.status(200).send({
+      message: "Login successful",
+      token: idToken,
+      provider: provider || "email",
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Email or password is incorrect",
     });
   }
 };
