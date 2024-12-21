58b6ba7a96987769a9255c6df80a52734dac951c
oceantran27 <daiduong3a1@gmail.com>
2024-12-21 09:08:59 +0700

    fix import

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,22 +1,20 @@
 // pages/index.js
-import React from 'react';
-import Hero from '../components/Hero';
-import Destination from '../components/Destination';
-import About from "../components/About";
-import Review from "../components/Review";
-import Benefit from "../components/Benefit";
+import React from "react";
+import Hero from "../components/Hero";
+import Destination from "../components/destination";
+import About from "../components/about";
+import Review from "../components/Rrview";
+import Benefit from "../components/benefit";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
-
 function Home() {
-
   useEffect(() => {
     AOS.init({
       duration: 1500,
     });
-}, []);
+  }, []);
   return (
     <div>
       <Hero />
